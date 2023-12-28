7c281174512aecf76a2e67d4616f494e1cb819bb
ParthasarathiMahana <parthasarathimahana@gmail.com>
2023-12-28 16:27:07 +0530

    student doubt form created with firebase.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import StudentDoubtForm from './components/StudentDoubtForm'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+function App() {
+
+  return (
+    <>
+      <StudentDoubtForm/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/StudentDoubtForm.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import styles from '../style/studentdoubtForm.module.css'
+import {db} from '../firebase/config'
+import { useState, useEffect } from 'react'
+import { addDoc, collection, setDoc, doc, getDocs, onSnapshot, deleteDoc} from 'firebase/firestore'
+
+const StudentDoubtForm = () => {
+
+    const [name, setName] = useState('')
+    const [batch, setBatch] = useState('')
+    const [subject, setSubject] = useState('')
+    const [topic, setTopic] = useState('')
+    const [doubt, setDoubt] = useState('')
+    const [mode, setMode] = useState('')
+
+    async function handleSubmit(){
+        console.log(name, batch, subject, topic, doubt, mode);
+        await addDoc(collection(db, "doubts"), {
+            student: name,
+            date: new Date(),
+            doubt,
+            subject,
+            topic,
+            "mode of resolution":mode
+          });
+          setName('')
+          setBatch('')
+          setSubject('')
+          setTopic('')
+          setDoubt('')
+          setMode('')
+    }
+
+    useEffect(()=>{
+
+    },[])
+
+
+  return (
+    <div className={styles.mainContainer}>
+      <input type="text" placeholder='Name' value={name} onChange={e=>setName(e.target.value)}/>
+      <input type="text" placeholder='Enter batch number' onChange={e=>setBatch(e.target.value)} value={batch}/>
+      <input type="text" placeholder='Subject' onChange={e=>setSubject(e.target.value)} value={subject}/>
+      <input type="text" placeholder='Topic' onChange={e=>setTopic(e.target.value)} value={topic}/>
+      <textarea name="" id="" cols="30" rows="10" placeholder='Explain doubt in detail' onChange={e=>setDoubt(e.target.value)} value={doubt}></textarea>
+      <input type="text" placeholder='Need 1 on 1, or text reply' onChange={e=>setMode(e.target.value)} value={mode}/>
+      <button onClick={handleSubmit}>Raise the doubt</button>
+    </div>
+  )
+}
+
+export default StudentDoubtForm
